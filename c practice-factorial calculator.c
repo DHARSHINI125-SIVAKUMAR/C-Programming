d3aa19061a9101280c2974bf94c25bec699b566b
DHARSHINI125-SIVAKUMAR <ddharshu894@gmail.com>
2026-02-17 09:47:38 +0530

    Add files via upload

---
+++ c practice-factorial calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int N;
+    int factorial=1;
+    scanf("%d",&N);
+    for(int i=1;i<=N;i++){
+        factorial=factorial*i;
+}
+printf("%d",factorial);
+return 0;
+}
