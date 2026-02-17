b35f78dbcffe0cf5590ec47cce3263ece429b4c8
DHARSHINI125-SIVAKUMAR <ddharshu894@gmail.com>
2026-02-17 12:03:46 +0530

    Add files via upload

---
+++ c practice- count prime numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int count = 0;
+
+    scanf("%d", &N);
+
+    
+    for(int i = 2; i <= N; i++) {
+        int isPrime = 1; 
+
+       
+        for(int j = 2; j < i; j++) {
+            if(i % j == 0) {
+                isPrime = 0; 
+                break;
+            }
+        }
+
+        if(isPrime) {
+            count++; 
+        }
+    }
+
+    
+    printf("%d\n", count);
+
+    return 0;
+}
