c8f9473e033f62117f279230ca85266d639b4595
DHARSHINI125-SIVAKUMAR <ddharshu894@gmail.com>
2026-02-20 17:33:22 +0530

    Add files via upload

---
+++ c practice-Delivery delay risk system.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int delay;
+    int totalDelay = 0;
+    int delayedCount = 0;
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &delay);
+
+        totalDelay += delay;
+
+        // Count deliveries delayed more than 30 minutes
+        if(delay > 30) {
+            delayedCount++;
+        }
+    }
+
+    printf("Total Delay: %d\n", totalDelay);
+    printf("Delayed Deliveries: %d\n", delayedCount);
+
+    return 0;
+}
