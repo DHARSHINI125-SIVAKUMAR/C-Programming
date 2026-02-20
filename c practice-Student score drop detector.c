70831665fa5b9f1a345e59ff58fa70ac53d09938
DHARSHINI125-SIVAKUMAR <ddharshu894@gmail.com>
2026-02-20 10:06:47 +0530

    Add files via upload

---
+++ c practice-Student score drop detector.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int N;
+    int marks;
+    int total = 0;
+    int failedSubjects = 0;
+    int average;
+
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &marks);
+
+        total = total + marks;   
+
+        if(marks < 40) {        
+            failedSubjects++;
+        }
+    }
+
+    average = total / N;
+
+    printf("Average Score: %d\n", average);
+    printf("Failed Subjects: %d\n", failedSubjects);
+
+    return 0;
+}
