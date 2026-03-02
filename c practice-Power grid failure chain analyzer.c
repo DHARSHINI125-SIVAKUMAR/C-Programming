e276d16e3e07d6ee3d65ac45c81ccdaa91f150d0
DHARSHINI125-SIVAKUMAR <ddharshu894@gmail.com>
2026-03-02 11:49:22 +0530

    Add files via upload

---
+++ c practice-Power grid failure chain analyzer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int capacity, N;
+    scanf("%d", &capacity);
+    scanf("%d", &N);
+
+    int i = 0;
+    int load;
+    int safeHours = 0;
+    int failureCount = 0;
+
+    while (i < N) {
+        scanf("%d", &load);
+
+        if (load > capacity) {
+            failureCount++;
+        } else {
+            safeHours++;
+        }
+
+        i++;
+    }
+
+    printf("Safe Hours: %d\n", safeHours);
+    printf("Failure Count: %d\n", failureCount);
+
+    return 0;
+}
